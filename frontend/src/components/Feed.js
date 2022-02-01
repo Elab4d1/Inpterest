4f245e79595a4e1069eb51c5ce6f34e6c9053390
Youssef ZAHI <67647543+youzh00@users.noreply.github.com>
2022-02-01 03:26:58 +0100

    Add files via upload

---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+import { feedQuery, searchQuery } from "../utils/data";
+
+const Feed = () => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || "new";
+  if (loading) {
+    return <Spinner message={`We are adding ${ideaName} pins to your feed!`} />;
+  }
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
